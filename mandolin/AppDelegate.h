378d57bf2f629a8febcc4324f4c975b8d267dd23
Nicholas Cardinal <nodigga@Nicholass-MacBook-Pro-3.local>
2015-03-03 21:34:17 -0800

    Initial Commit

---
+++ mandolin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mandolin
+//
+//  Created by Nicholas Cardinal on 3/3/15.
+//  Copyright (c) 2015 nodigga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mandolin/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mandolin
+//
+//  Created by Nicholas Cardinal on 3/3/15.
+//  Copyright (c) 2015 nodigga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
